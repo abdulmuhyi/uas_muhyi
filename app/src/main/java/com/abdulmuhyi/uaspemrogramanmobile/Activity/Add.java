5d014d3d2c595ee5203d0dbf4574b51894d86d4c
abdulmuhyi <muhyiabdul010599@gmail.com>
2021-07-24 08:47:56 +0700

    first commit

---
+++ app/src/main/java/com/abdulmuhyi/uaspemrogramanmobile/Activity/Add.java
@@ -0,0 +1,16 @@
+package com.abdulmuhyi.uaspemrogramanmobile.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abdulmuhyi.uaspemrogramanmobile.R;
+
+public class Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
